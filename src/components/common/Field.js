5fd176342f527d3dd9c44bdf05b3e9b0ad83b19d
Javier Ferrer <javier.f.g@um.es>
2019-05-25 21:01:14 +0100

    "$1"

--- src/components/common/Field.js
+++ src/components/common/Field.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList } from '@fortawesome/free-solid-svg-icons';
+
 
 const Field = ({name}) =>
-    <span className="badge badge-field border border-dark text-light mr-2" key={name}>{name}</span>
+  <span className="badge badge-field border border-dark text-light mr-2" key={name}>
+    <FontAwesomeIcon icon={faList} className="mr-1" /> {name}
+  </span>
 
 
 export default Field;
--- src/components/common/Service.js
+++ src/components/common/Service.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCubes } from '@fortawesome/free-solid-svg-icons';
+
 
 const Service = ({name}) =>
-  <span className="badge badge-service border border-dark text-dark" key={name}>{name}</span>
+  <span className="badge badge-service border border-dark text-dark" key={name}>
+    <FontAwesomeIcon icon={faCubes} className="mr-1" />
+    {name}
+  </span>
 
 
 export default Service;
