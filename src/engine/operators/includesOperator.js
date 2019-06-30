a9992949793333ac1dcd0900c1582472464849a6
jferrer <jferrer@visualtis.com>
2019-06-30 17:07:58 +0100

    partial match on includes operator

--- src/engine/operators/includesOperator.js
+++ src/engine/operators/includesOperator.js
@@ -4,16 +4,21 @@ import { config } from '../../config/Config';
 
 export const includesOperator = function (rule, relevantValues, item) {
   const matchingValues = rule.values.find(ruleValue => relevantValues.includes(ruleValue));
+  const partialMatchingValues = rule.values.find(ruleValue => {
+    const partialMatches = relevantValues.map(relevantValue => relevantValue.includes(ruleValue));
+    return partialMatches.includes(true);
+  });
   const hasMatchingValues = typeof matchingValues !== 'undefined';
+  const hasPartialMatchingValues = typeof partialMatchingValues !== 'undefined';
 
   config.debugRankingEngine && console.log('matchingValues', matchingValues, matchingValues ? 'SO, TRUE' : 'SO, FALSE');
 
   return {
     item: item.name,
     field: rule.field,
-    result: hasMatchingValues,
+    result: hasMatchingValues || hasPartialMatchingValues,
     rule: rule.name,
-    score: hasMatchingValues ? rule.importance : 0,
+    score: hasMatchingValues ? rule.importance : hasPartialMatchingValues ? rule.importance * .5 : 0,
     textPositive: 'includes',
     textNegative: 'does not include',
     values: rule.values
